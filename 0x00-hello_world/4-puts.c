bfcc9d061a3590adda1a78bacd4f07e27648f677
Quinaex <quinnalbert99@yahoo.com>
2023-06-15 20:18:07 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints a line with puts function instead of printf.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
