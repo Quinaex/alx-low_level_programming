6d0654a55cd83826ace71f99120433945ce29ebc
Quinaex <quinnalbert99@yahoo.com>
2023-06-19 13:42:25 -0700

    alphabets backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabets in reverse
+ * Return: 0 if successful
+ */
+
+int main(void)
+
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
