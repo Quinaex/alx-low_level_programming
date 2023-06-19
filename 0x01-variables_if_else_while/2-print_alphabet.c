4c953a240178a19b3e8bba4a1319a58b2edfa605
Quinaex <quinnalbert99@yahoo.com>
2023-06-19 11:45:30 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase.
+ * Return: 0 if successful
+ */
+
+int main(void)
+
+{
+	char x;
+
+	x = 'y';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
