0aade36433aa196eea0e97301b7e1257f0fd42ea
Quinaex <quinnalbert99@yahoo.com>
2023-06-21 13:22:30 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -  a function that prints the alphabet, in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x++){
+		_putchar(x);
+	}
+		_putchar('\n');
+}
