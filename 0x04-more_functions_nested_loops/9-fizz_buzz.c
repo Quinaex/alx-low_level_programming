2c3d382acc8c445e82352b468afd653a308bfd0b
Quinaex <quinnalbert99@yahoo.com>
2023-06-23 18:51:04 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the numbers from 1 to 100
+ * for multiples of 3 print fizz and for the multiples of 5 print Buzz
+ * For multiples of both 3 and 5 print FizzBuzz
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int q;
+
+	for (q = 1; q <= 100; q++)
+	{
+		if (q % 3 == 0 && q % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (q % 5 == 0 && q % 3 != 0)
+		{
+			printf(" Buzz");
+			if (q != 100)
+			{
+				printf(" ");
+			}
+		}
+		else if (q % 5 == 0 && q % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", q);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
